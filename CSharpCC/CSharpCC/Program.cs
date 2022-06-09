df498a35a45f414119568d4d622d7d91c0dfce27
Clarice Costello <claricostello@outlook.com>
2022-06-09 12:59:14 -0700

    initial app creation

---
+++ CSharpCC/CSharpCC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpCC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
